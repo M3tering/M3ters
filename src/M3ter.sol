a82d64d35210df1e4701224ef5da467e980d9a9e
ichristwin <ichristwin@gmail.com>
2024-04-06 16:49:00 +0100

    added minter role

--- src/M3ter.sol
+++ src/M3ter.sol
@@ -7,29 +7,34 @@ import "./ERC721ABC.sol";
 /// @custom:security-contact info@whynotswitch.com
 contract M3ter is ERC721ABC, IM3ter {
     bytes32 public constant REGISTRAR_ROLE = keccak256("REGISTRAR_ROLE");
-    uint256 private _nextTokenId;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 public nextTokenId;
 
-    mapping(uint256 => bytes32) public token_to_key;
-    mapping(bytes32 => uint256) public key_to_token;
+    mapping(uint256 => bytes32) public keyByToken;
+    mapping(bytes32 => uint256) public tokenByKey;
 
     constructor() ERC721("M3ter", "M3R") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(REGISTRAR_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri) public whenNotPaused {
-        uint256 tokenId = _nextTokenId++;
+    function safeMint(
+        address to,
+        string memory uri
+    ) external onlyRole(MINTER_ROLE) whenNotPaused {
+        uint256 tokenId = nextTokenId++;
         _setTokenURI(tokenId, uri);
         _safeMint(to, tokenId);
     }
 
-    function _register(
+    function register(
         uint256 tokenId,
         bytes32 publicKey
-    ) external onlyRole(REGISTRAR_ROLE) {
-        token_to_key[tokenId] = publicKey;
-        key_to_token[publicKey] = tokenId;
+    ) external onlyRole(REGISTRAR_ROLE) whenNotPaused {
+        keyByToken[tokenId] = publicKey;
+        tokenByKey[publicKey] = tokenId;
         emit Register(tokenId, publicKey, msg.sender, block.timestamp);
     }
 }
