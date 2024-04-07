9711403f131bb46bd93b7f9ddaef729542a452bd
ichristwin <ichristwin@gmail.com>
2024-04-07 16:02:38 +0100

    returned minter role

--- src/M3ter.sol
+++ src/M3ter.sol
@@ -7,6 +7,7 @@ import "./ERC721ABC.sol";
 /// @custom:security-contact info@whynotswitch.com
 contract M3ter is ERC721ABC, IM3ter {
     bytes32 public constant REGISTRAR_ROLE = keccak256("REGISTRAR_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 public nextTokenId;
 
     mapping(uint256 => bytes32) public keyByToken;
@@ -15,10 +16,14 @@ contract M3ter is ERC721ABC, IM3ter {
     constructor() ERC721("M3ter", "M3R") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(REGISTRAR_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri) external whenNotPaused {
+    function safeMint(
+        address to,
+        string memory uri
+    ) external onlyRole(MINTER_ROLE) whenNotPaused {
         uint256 tokenId = nextTokenId++;
         _setTokenURI(tokenId, uri);
         _safeMint(to, tokenId);
